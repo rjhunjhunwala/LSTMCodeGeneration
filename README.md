f0c9c5eb166c3d12b9f202010e457ebcb0d5099c
Rohan Jhunjhunwala <rjhunjhunwala80@berkeley.edu>
2018-12-26 08:32:10 -0500

    Updated ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # LSTMCodeGeneration
+
 Teaching my mac to automate me out of my Job!
+
+We seek to build a character based LSTM model, to naively model software developement as a language problem. Simply put, we seek to guess the next character of a computer program, given the history.
---
+++ TrainCodingModel.py
@@ -0,0 +1,31 @@
+import numpy as np
+from pickle import dump
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+INPUT_SEQUENCE_LENGTH = 100
+VOCAB_SIZE = 256
+
+X = []
+y = []
+s = ""
+with open("code.txt","r") as fil:
+    s = fil.read()
+for i in range(0,len(s)-INPUT_SEQUENCE_LENGTH-1):
+    X.append([ord(a) for a in s[i:i+INPUT_SEQUENCE_LENGTH]])
+    y.append(ord(s[i+INPUT_SEQUENCE_LENGTH]))
+
+X = [[[int(i == el) for i in range(VOCAB_SIZE)]for el in seq] for seq in X]
+y = [[int(i == el) for i in range(VOCAB_SIZE)] for el in y]
+
+X = np.array(X)
+y = np.array(y)
+model = Sequential()
+model.add(LSTM(125,input_shape = (INPUT_SEQUENCE_LENGTH, VOCAB_SIZE)))
+model.add(LSTM(125,input_shape = (INPUT_SEQUENCE_LENGTH, VOCAB_SIZE)))
+model.add(Dense(VOCAB_SIZE, activation='softmax'))
+print(model.summary())
+model.compile(loss='categorical_crossentropy',optimizer='adam')
+model.fit(X ,y,epochs = 100, verbose = 2)
+model.save("alphabetmodel.h5")
