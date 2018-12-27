b7a99c59a803e16f4b47ba781af777d5059201a0
rjhunjhunwala <rjhunjhunwala80@berkeley.edu>
2018-12-26 19:20:05 -0800

    Trained

--- test_model.py
+++ test_model.py
@@ -9,10 +9,10 @@ input_sequence = [dummy_input[i%len(dummy_input)] for i in range(input_length)]
 model = keras.models.load_model(file_name)
 
 for _ in range(int(input("Characters to generate"))):
+
     prediction = np.array([[[i == el for i in range(256)] for el in input_sequence]])
-    print(prediction.shape)
     input_sequence+=[model.predict_classes(prediction, verbose = 0)]
-    print(chr(input_sequence[-1]))
+    print(chr(input_sequence[-1]),end = '')
     input_sequence = input_sequence[1:]
 
 
