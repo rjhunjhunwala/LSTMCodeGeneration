6331c810ca3a5f4264174f0d81fad252d8cc392f
rjhunjhunwala <rjhunjhunwala80@berkeley.edu>
2018-12-27 23:01:07 -0500

    Done, it produces some sort of output

--- test_model.py
+++ test_model.py
@@ -7,16 +7,22 @@ dummy_input = [ord(dum) for dum in input("Dummy Input?")]
 
 input_sequence = [dummy_input[i%len(dummy_input)] for i in range(input_length)]
 model = keras.models.load_model(file_name)
-
+out = ''
 for _ in range(int(input("Characters to generate"))):
 
-    prediction = np.array([[[i == el for i in range(256)] for el in input_sequence]])
-    input_sequence+=[model.predict_classes(prediction, verbose = 0)]
-    print(chr(input_sequence[-1]),end = '')
-    input_sequence = input_sequence[1:]
+    try:
+        prediction = np.array([[[int(i == el) for i in range(256)] for el in input_sequence]])
+        # print(prediction)
+        input_sequence+=[model.predict_classes(prediction, verbose = 0)]
+        # print("SUCCEDED GOT:",end = '')
+        out+=chr(input_sequence[-1])
+        input_sequence = input_sequence[1:]
+    except:
+        print("FAILED ON!")
+        print(prediction)
 
-
-
-
+print(dummy_input)
+print("---predicted rest---")
+print(out)
 
 
